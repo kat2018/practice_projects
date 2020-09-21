3f50912a96df496d56b0ad3c708062eb881b3fc7
Katia Eidson <katia.eidson@coxautoinc.com>
2020-09-21 12:55:11 -0400

    added second module

---
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -0,0 +1,15 @@
+// // let can be changed
+// let userName = 'Max';
+// userName = 'Manu';
+
+// console.log(userName);
+
+// // const cant be changed
+// const totalUsers = 15;
+// totalUsers = 20;
+
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+outputResult(currentResult, '');
