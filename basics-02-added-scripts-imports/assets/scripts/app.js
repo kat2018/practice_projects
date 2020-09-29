ef57d968fa2b46b1c481445b074e4b733a974da7
Katia Eidson <katia.eidson@coxautoinc.com>
2020-09-29 12:44:33 -0400

    reverted error message example

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -4,8 +4,6 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 
-const anErrror = 'An error \n' + 'occured';
-
-outputResult(currentResult, anErrror);
+outputResult(currentResult, calculationDescription);
